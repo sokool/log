e7f60ab47638962ab8f7c43196d687b7118620ac
sokool <marian.sokolowski@gmail.com>
2023-04-03 13:52:04 +0200

    new message implementation

--- log_test.go
+++ log_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestNew(t *testing.T) {
 	var b bytes.Buffer
-	log.New(&b, log.Type|log.Tag|log.Colors).Tag("test").Printf("new:err system %s", "failure")
-	if s := b.String(); s != "[\u001B[31;1mERR\u001B[0m] [\u001B[36;1mtest:new\u001B[0m] system failure\n" {
+	log.New(&b, log.Type|log.Tag|log.Colors).Tag("log").Printf("new:err system %s", "failure")
+	if s := b.String(); s != "[\u001B[31;1mERR\u001B[0m] [\u001B[36;1mlog:new\u001B[0m] system failure\n" {
 		t.Fatal()
 	}
 }
